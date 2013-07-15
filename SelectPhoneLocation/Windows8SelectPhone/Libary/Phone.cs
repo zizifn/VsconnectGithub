59085d7fd2fc7e6a05d9df494f30ea34cb02ed26
zizifn <515868058@qq.com>
2013-07-15 19:53:04 +0800

    Add the binding element

---
+++ SelectPhoneLocation/Windows8SelectPhone/Libary/Phone.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows8SelectPhone.Libary
+{
+    public class Phone
+    {
+        public string PhoneNumber { set; get; }
+        public string PhoneLocation { set; get; }
+    }
+}
--- SelectPhoneLocation/Windows8SelectPhone/MainPage.xaml.cs
+++ SelectPhoneLocation/Windows8SelectPhone/MainPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows8SelectPhone.Libary;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
@@ -21,9 +22,12 @@ namespace Windows8SelectPhone
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Phone phone=new Phone();
         public MainPage()
         {
             this.InitializeComponent();
+       
+          
         }
 
         /// <summary>
@@ -33,6 +37,24 @@ namespace Windows8SelectPhone
         /// 属性通常用于配置页。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            phone.PhoneNumber = "1234";
+                 DataContext = phone;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string phonenumber = phone.PhoneNumber;
+            this.PhoneLocation.Text = phonenumber + "位置";
+        }
+
+        private void PhoneLocation_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+           
         }
     }
 }
