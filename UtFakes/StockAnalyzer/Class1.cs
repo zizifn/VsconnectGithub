37e74a351bc147d9b03aae9fde43627451af8af8
zizifn <515868058@qq.com>
2013-11-13 23:34:19 +0800

    UT fakes project

---
+++ UtFakes/StockAnalyzer/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS.UTFakes
+{
+     public interface IStockFeed
+    {
+        int GetSharePrice(string company);
+    }
+
+    public class StockAnalyzer
+    {
+        private IStockFeed stockFeed;
+        public StockAnalyzer(IStockFeed feed)
+        {
+            stockFeed = feed;
+        }
+        public int GetContosoPrice()
+        {
+            return stockFeed.GetSharePrice("COOO");
+        }
+    }
+}
