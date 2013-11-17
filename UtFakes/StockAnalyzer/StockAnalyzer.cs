301f4817f7f60a8642cafbe33cd388e03d42f4dd
zizifn <515868058@qq.com>
2013-11-17 23:56:53 +0800

    update the shim

--- UtFakes/StockAnalyzer/StockAnalyzer.cs
+++ UtFakes/StockAnalyzer/StockAnalyzer.cs
@@ -6,14 +6,31 @@ using System.Threading.Tasks;
 
 namespace VS.UTFakes
 {
+    /// <summary>
+    /// 为Fake接口，做准备
+    /// </summary>
      public interface IStockFeed
     {
         int GetSharePrice(string company);
+        int Price { get; set; }
+
+        //event EventHandler Changed;
+         /// <summary>
+        /// generic methods 
+         /// </summary>
+         /// <typeparam name="T"></typeparam>
+         /// <returns></returns>
+        T GetValue<T>();
+
     }
 
+    /// <summary>
+     /// 为Fake接口，做准备
+    /// </summary>
     public class StockAnalyzer
     {
         private IStockFeed stockFeed;
+        public event EventHandler Changed;
         public StockAnalyzer(IStockFeed feed)
         {
             stockFeed = feed;
@@ -22,8 +39,26 @@ namespace VS.UTFakes
         {
             return stockFeed.GetSharePrice("COOO");
         }
+        public int GetIstockFeedPrice()
+        {
+            return stockFeed.Price;
+        }
+        public int SetIstockFeedPrice(int i)
+        {
+            stockFeed.Price = i;
+            return stockFeed.Price;
+        }
+        public int GetGenericmethods()
+        {
+            return stockFeed.GetValue<int>();
+        }
+
+
     }
 
+    /// <summary>
+    /// 此类为Fake 系统的DateTime.Now做准备
+    /// </summary>
     public class componentUnderTest
     {
         public int GetTheCurrentYear()
@@ -32,7 +67,9 @@ namespace VS.UTFakes
         }
     }
 
-    // code under test
+    /// <summary>
+    /// S.UTFakes.Fakes.ShimcomponentUnderTest.AllInstances.GetTheCurrentYear = (componentUnderTest) => { return 1; };
+    /// </summary>
     public static class Y2KChecker
     {
         public static void Check()
@@ -46,4 +83,15 @@ namespace VS.UTFakes
             return 5;
         }
     }
+
+    public class ClassMethod
+    {
+        public int return1 { set; get; }
+
+        public string returnClassMethodName()
+        {
+            return "ClassMethod";
+        }
+    }
+
 }
